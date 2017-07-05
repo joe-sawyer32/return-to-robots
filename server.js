34d0a5ad170371ad9912a73b80f8da415855972d
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-05 14:37:28 -0500

    User directory and user profiles rendering correctly

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 // IMPORTS
 const express = require("express");
-const app = require(express());
+const app = express();
+const mustacheExpress = require("mustache-express");
 const port = process.env.port || 8000;
 const path = require("path");
 
-const mustacheExpress = require("mustache-express");
-const users = require(path.join(__dirname, "/data.js"));
+// MONGO
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
+const dbUrl = "mongodb://localhost:27017/return-to-robotsDB";
+let DB;
+let USERS;
+MongoClient.connect(dbUrl, (error, db) => {
+  if (error) {
+    console.warn("Unable to establish connection to the database", error);
+  }
+  DB = db;
+  USERS = db.collection("users");
+});
 
 // SET VIEW ENGINE
 app.engine("mustache", mustacheExpress());
@@ -18,9 +28,24 @@ app.set("views", path.join(__dirname, "/views"));
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 //ROUTES
-app.get("/", (req, res) => {});
+app.get("/users", (req, res) => {
+  USERS.find({}).toArray((error, foundUsers) => {
+    if (error) {
+      res.status(500).send(error);
+    }
+    res.render("index", { userList: foundUsers });
+  });
+});
 
-app.get("/user/:id", (req, res) => {});
+app.get("/users/:id", (req, res) => {
+  let userId = parseInt(req.params.id);
+  USERS.findOne({ id: userId }, (error, foundUser) => {
+    if (error) {
+      res.status(500).send(error);
+    }
+    res.render("user", { user: foundUser });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Spinning with express: Port ${port}`);
